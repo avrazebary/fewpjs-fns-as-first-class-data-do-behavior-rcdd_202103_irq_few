74c7c2f20e2def8e27cd7a5a4294945f2f5ed77f
Avraz  Zebary <avrazebary@gmail.com>
2021-04-08 12:37:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ function greet(time){
   if(time>=12){
     console.log("Good Morning");
   }
-  else if(time>=12 && time <= )
+  else if(time>=12 && time <=17 ){
+     console.log("Good Afternoon");
+  }
+  else if (time>17){
+    console.log("Good Evening");
+  }
 }
 /* Write your implementation of displayMessage() */
