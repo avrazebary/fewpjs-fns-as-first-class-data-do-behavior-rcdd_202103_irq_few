330fa447342ca45df3fd6dfabee1e13a8a028afa
Avraz  Zebary <avrazebary@gmail.com>
2021-04-08 12:47:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 
 function greet(time){
-  var a  = hms.split(':');
+  var a  = time.split(':');
   
   if(time>=12){
     console.log("Good Morning");
